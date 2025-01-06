2cc87fd0e69091632061bc181e0250bf5c7c9bc2
dinesh <dlaller1988@gmail.com>
2025-01-06 14:57:08 +0530

    user page updated

--- src/app/components/User.tsx
+++ src/app/components/User.tsx
@@ -14,6 +14,20 @@ interface UserData {
 
 const User = () => {
   const [fetchData, setfetchData] = useState<UserData[]>([]);
+  // const [formData,setformData]=useState<any>();
+  const [week, setWeek] = useState<string>("");
+  const [time, setTime] = useState<string>("");
+  const [selectError, setselectError] = useState<string>("");
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    if (name === "week") {
+      setWeek(value);
+    } else if (name === "slot") {
+      setTime(value);
+    }
+  };
 
   const showToast = (type: "success" | "error" | "warn", message: string) => {
     toast[type](message, {
@@ -52,39 +66,70 @@ const User = () => {
     }
   };
 
-  return (
-    <div className="p-4">
-      <form>
-        <div className="flex flex-col md:flex-row justify-between items-center mb-4 p-4 border rounded-lg gap-4 bg-green-200 hover:bg-green-500">
-          {/* Weekday Select */}
-          <select name="week" className="p-2 border rounded w-full md:w-1/4">
-            <option value="">Select Weekday</option>
-            {fetchData.map((item) => (
-              <option key={item._id} value={item.weekday}>
-                {item.weekday}
-              </option>
-            ))}
-          </select>
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const url = process.env.NEXT_PUBLIC_API_URL;
+    const formData = { weekday: week, slotTime: time };
+    const response = await fetch(`${url}userdata`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+    if (response.ok) {
+      showToast("success", "Slot Added successfully")
+      const data = await response.json();
 
-          {/* Time Slot Select */}
-          <select name="slot" className="p-2 border rounded w-full md:w-1/4">
-            <option value="">Select Time</option>
-            {fetchData.map((item) => (
-              <option key={item._id} value={item.slotTime}>
-                {item.slotTime}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="bg-green-100 hover:bg-green-500 px-4 py-2 border-b rounded-md"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+      console.log("slot booking :", data);
+    } else {
+      const data = await response.json();
+      if (data.message === "weekname already exists") {
+        showToast("error","weekname already appointed")
+        setselectError("weekname already appointed");
+        
+      } else if (data.message === "slot already exists") {
+        showToast("error","Slot already booked")
+        setselectError("Slot already booked");
+      } else {
+        setselectError("An error occurred. Please try again.");
+      }
+    }
+  }
+
+    return (
+      <div className="p-4">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col md:flex-row justify-between items-center mb-4 p-4 border rounded-lg gap-4 bg-green-200 hover:bg-green-500">
+            {/* Weekday Select */}
+            <select name="week" value={week} onChange={handleChange} className="p-2 border rounded w-full md:w-1/4">
+              <option value="">Select Weekday</option>
+              {fetchData.map((item) => (
+                <option key={item._id} value={item.weekday}>
+                  {item.weekday}
+                </option>
+              ))}
+            </select>
+
+            {/* Time Slot Select */}
+            <select name="slot" value={time} onChange={handleChange} className="p-2 border rounded w-full md:w-1/4">
+              <option value="">Select Time</option>
+              {fetchData.map((item) => (
+                <option key={item._id} value={item.slotTime}>
+                  {item.slotTime}
+                </option>
+              ))}
+            </select>
+          </div>
+          {selectError}
+          <button
+            type="submit"
+            className="bg-green-100 hover:bg-green-500 px-4 py-2 border-b rounded-md"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+
+    );
+  }
 
 export default User;
